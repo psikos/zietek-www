92d4652d2d39db39f6aac2622d3e040231ff51fa
Mikolaj Szymanski <szymikolaj@gmail.com>
2020-08-05 19:00:55 +0200

    lot of fixes, wywoz-section new design

--- js/index.js
+++ js/index.js
@@ -14,8 +14,6 @@ function handleHamburgerClick(event) {
 
 //slider
 
-const prev = document.getElementById("prevPicture");
-const next = document.getElementById("nextPicture");
 const listOfImages = document.querySelectorAll(".slider ul li");
 let visibleElement = 0;
 
@@ -37,6 +35,32 @@ function handleNext(event) {
   }
   listOfImages[visibleElement].classList.add("visible");
 }
-// const idInterwalu = window.setInterval(handleNext, 2000);
-// prev.addEventListener('click', handlePrev);
-// next.addEventListener('click', handleNext);
+const idInterwalu = window.setInterval(handleNext, 3500);
+
+const listOfImages2 = document.querySelectorAll(".elem");
+console.log(listOfImages2);
+
+const prev = document.getElementById("prevPicture");
+const next = document.getElementById("nextPicture");
+let visibleElement2 = 0;
+
+listOfImages2[visibleElement2].classList.add("visible2");
+
+function handlePrev2(event) {
+  listOfImages2[visibleElement2].classList.remove("visible2");
+  visibleElement2--;
+  if (visibleElement2 < 0) {
+    visibleElement2 = listOfImages.length - 1;
+  }
+  listOfImages2[visibleElement2].classList.add("visible2");
+}
+function handleNext2(event) {
+  listOfImages2[visibleElement2].classList.remove("visible2");
+  visibleElement2++;
+  if (visibleElement2 > listOfImages2.length - 1) {
+    visibleElement2 = 0;
+  }
+  listOfImages2[visibleElement2].classList.add("visible2");
+}
+prev.addEventListener("click", handlePrev2);
+next.addEventListener("click", handleNext2);
