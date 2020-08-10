d3a7454d91e1718caf99dd7b1b3a60c5c96f8752
Mikolaj Szymanski <szymikolaj@gmail.com>
2020-08-10 20:41:10 +0200

    second slider arrow buttons

--- js/index.js
+++ js/index.js
@@ -2,15 +2,15 @@ const hamburger = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
 const navItems = document.querySelectorAll(".navigation-item a");
 
+const handleHamburgerClick = (event) => {
+  hamburger.classList.toggle("hamburger-active");
+  navigation.classList.toggle("navigation-active");
+};
+
 navItems.forEach((elem) =>
   elem.addEventListener("click", handleHamburgerClick)
 );
 hamburger.addEventListener("click", handleHamburgerClick);
-
-function handleHamburgerClick(event) {
-  hamburger.classList.toggle("hamburger-active");
-  navigation.classList.toggle("navigation-active");
-}
 
 //slider
 
@@ -19,26 +19,27 @@ let visibleElement = 0;
 
 listOfImages[visibleElement].classList.add("visible");
 
-function handlePrev(event) {
+const handlePrev = (event) => {
   listOfImages[visibleElement].classList.remove("visible");
   visibleElement--;
   if (visibleElement < 0) {
     visibleElement = listOfImages.length - 1;
   }
   listOfImages[visibleElement].classList.add("visible");
-}
-function handleNext(event) {
+};
+const handleNext = (event) => {
   listOfImages[visibleElement].classList.remove("visible");
   visibleElement++;
   if (visibleElement > listOfImages.length - 1) {
     visibleElement = 0;
   }
   listOfImages[visibleElement].classList.add("visible");
-}
+};
 const idInterwalu = window.setInterval(handleNext, 3500);
 
+//slider2
+
 const listOfImages2 = document.querySelectorAll(".elem");
-console.log(listOfImages2);
 
 const prev = document.getElementById("prevPicture");
 const next = document.getElementById("nextPicture");
@@ -46,21 +47,21 @@ let visibleElement2 = 0;
 
 listOfImages2[visibleElement2].classList.add("visible2");
 
-function handlePrev2(event) {
+const handlePrev2 = (event) => {
   listOfImages2[visibleElement2].classList.remove("visible2");
   visibleElement2--;
   if (visibleElement2 < 0) {
     visibleElement2 = listOfImages.length - 1;
   }
   listOfImages2[visibleElement2].classList.add("visible2");
-}
-function handleNext2(event) {
+};
+const handleNext2 = (event) => {
   listOfImages2[visibleElement2].classList.remove("visible2");
   visibleElement2++;
   if (visibleElement2 > listOfImages2.length - 1) {
     visibleElement2 = 0;
   }
   listOfImages2[visibleElement2].classList.add("visible2");
-}
+};
 prev.addEventListener("click", handlePrev2);
 next.addEventListener("click", handleNext2);
