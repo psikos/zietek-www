7e69b9f3307947aea20220983bc2a63644ad72fd
Mikolaj Szymanski <szymikolaj@gmail.com>
2020-09-30 23:03:37 +0200

    cookies added, all completed

--- js/index.js
+++ js/index.js
@@ -131,3 +131,49 @@ const handleSubmit = (event) => {
 };
 
 submit.addEventListener("click", handleSubmit);
+
+//cookies
+//funkcja ustawiania pliku cookie
+const setCookie = (cname, cvalue, exdays) => {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+};
+
+//funkcja odczytywania pliku cookie
+const getCookie = (cname) => {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    const c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+};
+
+//funckja sprawdzania pliku cookie
+const checkCookie = () => {
+  var user = getCookie("username");
+  if (user != "") {
+    return null;
+  } else {
+    username = "user";
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+};
+
+const cookie = document.querySelector(".cookie");
+const cookieButton = document.querySelector(".cookie-button");
+
+cookieButton.addEventListener("click", () => {
+  cookie.classList.add("clicked");
+});
